da0144d4d06d4ff3767aefc2c43581aadc351076
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-04-14 18:34:10 +0200

    added async function in index.js to fetch data from the api and create elements and classes based on previous hard coded elements in index.html, added for .myCard in all-movies.css

---
+++ js/index.js
@@ -0,0 +1,36 @@
+fetch('https://topwarmovies.onrender.com/api/v1/warmovies')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('data-container');
+    data.forEach(movie => {
+      const imageUrl = movie.media.imageUrl.startsWith('/')
+        ? `https://topwarmovies.onrender.com${movie.media.imageUrl}`
+        : movie.media.imageUrl;
+
+      const card = document.createElement('div');
+      card.className = 'myCard p-5 is-rounded-xl';
+      card.style.backgroundImage = `url('${imageUrl}')`;
+
+      card.innerHTML = `
+        <section class="is-flex is-flex-direction-column is-align-items-center is-justify-content-center">
+          <h3 class="myCard-bg myCard-radius myCard-title has-text-light is-size-4 has-background-black has-text-centered px-3 is-rounded">
+            ${movie.title}
+          </h3>
+          <h4 class="myCard-bg myCard-plot is-size-5 mt-5">Plot</h4>
+          <p class="myCard-bg myCard-txt is-size-6 has-text-left">
+            ${movie.plot}
+          </p>
+          <p class="myCard-bg myCard-radius is-size-4 my-4 px-4">
+            User Rating: <span class="has-text-success">${movie.imdbRating.userRating}</span>
+          </p>
+          <button type="button" class="my-button button is-danger is-large">
+            More Details &gt;
+          </button>
+        </section>
+      `;
+
+      // Lägg till kortet i containern
+      container.appendChild(card);
+    });
+  })
+  .catch(error => console.error('Error:', error));
